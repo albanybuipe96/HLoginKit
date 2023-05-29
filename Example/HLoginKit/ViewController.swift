ba3f33447d95a291e915ac19a1038da7d1bd8548
albanybuipe96 <mohammedarahman96@gmail.com>
2023-05-29 15:46:52 +0000

    login view added

--- Example/HLoginKit/ViewController.swift
+++ Example/HLoginKit/ViewController.swift
@@ -7,12 +7,82 @@
 //
 
 import UIKit
+import HLoginKit
 
 class ViewController: UIViewController {
+    
+    private lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textAlignment = .center
+        label.text = "Financio Inc."
+        label.font = UIFont(name: "Helvetica", size: 55)
+        label.textColor = .systemGray2
+        self.view.addSubview(label)
+        return label
+    }()
+    
+    private lazy var mottoLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textAlignment = .center
+        label.text = "Empowering Your Financial Journey!"
+        label.font = UIFont(name: "Times New Roman", size: 14)
+        self.view.addSubview(label)
+        return label
+    }()
+    
+    lazy var loginView: LoginView = {
+        let view = LoginView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(view)
+        return view
+    }()
+    
+    private lazy var signinButton: UIButton = {
+        let signinButton = UIButton(type: .system)
+        signinButton.translatesAutoresizingMaskIntoConstraints = false
+        if #available(iOS 15.0, *) {
+            signinButton.configuration = .filled()
+        } else {
+            // Fallback on earlier versions
+            signinButton.layer.borderWidth = 1
+            signinButton.layer.borderColor = UIColor.gray.cgColor
+        }
+        if #available(iOS 15.0, *) {
+            signinButton.configuration?.imagePadding = 8
+        } else {
+            // Fallback on earlier versions
+        }
+        signinButton.setTitle("Sign in", for: [])
+        signinButton.addTarget(self, action: #selector(signinTapped), for: .primaryActionTriggered)
+        signinButton.layer.cornerRadius = 5
+        signinButton.clipsToBounds = true
+        self.view.addSubview(signinButton)
+        return signinButton
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        NSLayoutConstraint.activate([
+            loginView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            loginView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            loginView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 8),
+            loginView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -8),
+            
+            titleLabel.bottomAnchor.constraint(equalTo: loginView.topAnchor, constant: -50),
+            titleLabel.leadingAnchor.constraint(equalTo: loginView.leadingAnchor),
+            titleLabel.trailingAnchor.constraint(equalTo: loginView.trailingAnchor),
+            
+            mottoLabel.topAnchor.constraint(equalToSystemSpacingBelow: titleLabel.bottomAnchor, multiplier: -20),
+            mottoLabel.leadingAnchor.constraint(equalTo: loginView.leadingAnchor),
+            mottoLabel.trailingAnchor.constraint(equalTo: loginView.trailingAnchor),
+            
+            signinButton.topAnchor.constraint(equalToSystemSpacingBelow: loginView.bottomAnchor, multiplier: 2),
+            signinButton.leadingAnchor.constraint(equalTo: loginView.leadingAnchor),
+            signinButton.trailingAnchor.constraint(equalTo: loginView.trailingAnchor),
+            
+        ])
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,5 +90,8 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @objc func signinTapped(_ action: UIAction?) {
+       print("Signin Tapped!")
+    }
 }
 
---
+++ Sources/LoginView.swift
@@ -0,0 +1,106 @@
+//
+//  LoginView.swift
+//  UiKitBankApp
+//
+//  Created by Albany Buipe on 22/05/2023.
+//
+
+import Foundation
+import UIKit
+
+public class LoginView: UIView {
+    
+    public lazy var usernameTextField: UITextField = {
+        let textfield = UITextField()
+        textfield.translatesAutoresizingMaskIntoConstraints = false
+        textfield.placeholder = "Username"
+//        textfield.text = "Albany"
+        textfield.delegate = self
+        return textfield
+    }()
+    
+    public lazy var passwordTextField: UITextField = {
+        let textfield = UITextField()
+        textfield.translatesAutoresizingMaskIntoConstraints = false
+        textfield.placeholder = "Password"
+//        textfield.text = "welcome"
+        textfield.isSecureTextEntry = true
+        textfield.delegate = self
+        return textfield
+    }()
+    
+    private lazy var divider: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .white
+        view.heightAnchor.constraint(equalToConstant: 3).isActive = true
+        return view
+    }()
+    
+    private lazy var stack: UIStackView = {
+        let view = UIStackView(arrangedSubviews: [
+            usernameTextField,
+            divider,
+            passwordTextField
+        ])
+        
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.axis = .vertical
+        view.spacing = 8
+        return view
+    }()
+
+    
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        translatesAutoresizingMaskIntoConstraints = false
+        backgroundColor = .secondarySystemBackground
+        
+        layer.cornerRadius = 5
+        clipsToBounds = true
+        
+        layout()
+    }
+    
+    public required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
+
+extension LoginView {
+    
+    func layout() {
+        
+        addSubview(stack)
+        
+        NSLayoutConstraint.activate([
+            stack.topAnchor.constraint(equalToSystemSpacingBelow: topAnchor, multiplier: 1),
+            stack.leadingAnchor.constraint(equalToSystemSpacingAfter: leadingAnchor, multiplier: 1),
+            trailingAnchor.constraint(equalToSystemSpacingAfter: stack.trailingAnchor, multiplier: 1),
+            bottomAnchor.constraint(equalToSystemSpacingBelow: stack.bottomAnchor, multiplier: 1),
+        ])
+        
+    }
+}
+
+// MARK: - UITextFieldDelegate
+extension LoginView: UITextFieldDelegate {
+    
+    public func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        usernameTextField.endEditing(true)
+        passwordTextField.endEditing(true)
+        
+        return true
+    }
+    
+    public func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        return true
+    }
+    
+    public func textFieldDidEndEditing(_ textField: UITextField) {
+        
+    }
+    
+}
